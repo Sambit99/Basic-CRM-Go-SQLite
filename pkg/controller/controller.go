1a19454569471cf786582c108a5461e9ceaff1d4
Sambit99 <sambitkumar75@gmail.com>
2025-09-03 21:02:59 +0530

    feat: add controller functions for lead management

---
+++ pkg/controller/controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetLeads(c *fiber.Ctx) error {
+	return nil
+}
+func GetLead(c *fiber.Ctx) error {
+	return nil
+}
+func NewLead(c *fiber.Ctx) error {
+	return nil
+}
+func DeleteLead(c *fiber.Ctx) error {
+	return nil
+}
