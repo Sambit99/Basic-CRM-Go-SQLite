afa2f6d7b81bcbd435c87d21ea795fa331cd073a
Sambit99 <sambitkumar75@gmail.com>
2025-09-08 22:05:40 +0530

    feat: implement GetLeads function to retrieve all leads from the database

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,18 +1,28 @@
 package controller
 
 import (
+	"github.com/Sambit99/Basic-CRM-Go-SQLite/pkg/model"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetLeads(c *fiber.Ctx) error {
-	return nil
-}
-func GetLead(c *fiber.Ctx) error {
-	return nil
-}
-func NewLead(c *fiber.Ctx) error {
-	return nil
-}
-func DeleteLead(c *fiber.Ctx) error {
-	return nil
+	leads, err := model.GetLeads()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to retrieve leads",
+		})
+	}
+
+	if len(leads) == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "No Leads Found",
+		})
+	}
+
+	c.Set("Content-Type", "application/json")
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status": "success",
+		"data":   leads,
+	})
 }
