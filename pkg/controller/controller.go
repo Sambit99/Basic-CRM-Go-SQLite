fe10f44398eee1d2f3230a527c76c49442e511d0
Sambit99 <sambitkumar75@gmail.com>
2025-09-08 22:06:14 +0530

    feat: add GetLead function to retrieve a lead by ID

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"strconv"
+
 	"github.com/Sambit99/Basic-CRM-Go-SQLite/pkg/model"
 	"github.com/gofiber/fiber/v2"
 )
@@ -24,5 +26,38 @@ func GetLeads(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"status": "success",
 		"data":   leads,
+	})
+}
+
+func GetLead(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	parsedId, err := strconv.ParseInt(id, 0, 0)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid ID Format",
+		})
+	}
+
+	lead, err := model.GetLead(int(parsedId))
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to retrieve lead",
+		})
+	}
+
+	if lead.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Lead Not found",
+		})
+	}
+
+	c.Set("Content-Type", "application/json")
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status": "success",
+		"data":   lead,
 	})
 }
